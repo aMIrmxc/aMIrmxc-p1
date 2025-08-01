7e2ad24149dfdee00cc82be3b6e3bb4808d6ae04
aMIrmxc <amirmxc@gmail.com>
2025-08-01 23:26:31 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+export default function App() {
+  return (
+    <div className="container">
+      <div className="scale-on-hover">
+        <img src="/amirmxc.jpg" alt="logo" className="logo animate-float" />
+      </div>
+       
+        <h1>Front-End Developer</h1>
+        <p>
+           A front-end developer with a passion for creating beautiful and
+          functional web applications.
+        </p>
+
+      <div className="social-links">
+        <a
+          href="https://github.com/aMIrmxc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://linkedin.com/in/amirmxc/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a href="mailto:your.email@example.com">Email</a>
+
+        <a
+          href="https://t.me/Amirmxc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Telegram
+        </a>
+      </div>
+    </div>
+  );
+}
